7acf4f935b00d4539d1a00dfe148e8f7b272b44d
ccchibsss <ccchibsss@gmail.com>
2026-05-03 12:51:02 +0300

    Update utils.py

--- workflow_builder_pro/utils.py
+++ workflow_builder_pro/utils.py
@@ -266,6 +266,7 @@ def initialize_session_state():
         'uploaded_images': {},
         'processed_images': {},
         'image_batch_progress': 0,
+        'chat_input': '',                      # <--- ДОБАВЛЕНО для исправления ошибки
     }
     
     for key, value in defaults.items():
